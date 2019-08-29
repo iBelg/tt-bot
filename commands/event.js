f011826093f646862219f760e2df524896d22157
Glitch (melodious-cricket) <none>
2019-08-29 12:53:44 +0000

    Another update from glitch

--- commands/event.js
+++ commands/event.js
@@ -4,11 +4,11 @@ exports.run = (client, message, args) => {
   const embed = new Discord.RichEmbed()
       .setTitle(`${message.author.username} created an event!`)
       .setColor(0xFF0000)
-      .setDescription(...args);
+      .setDescription(args.join(' '));
     message.channel.send('@everyone A new event has been created.');
     message.channel.send(embed)
-      .then((message) => {
-        message.react('✅');
-        message.react('❎');
+      .then(async (message) => {
+        await message.react('✅');
+        await message.react('❎');
       });
 };
