b815291a45e22386286fddb6d97874333616cee8
Giovani Vercauteren <giovani.vercauteren@gmail.com>
2019-12-17 15:12:00 +0100

    move command

---
+++ commands/move.js
@@ -0,0 +1,24 @@
+exports.usage = (client) => {
+    return `
+    ${client.config.prefix}move <user mention>
+    `;
+};
+
+exports.run = (client, message) => {
+    const mentionedUsers = message.mentions.users;
+    const authorUser = message.author;
+    const authorMember = message.guild.member(authorUser);
+    const toVoiceChannel = authorMember.voiceChannel;
+    if (mentionedUsers && mentionedUsers.size > 0) {
+        mentionedUsers.forEach((user) => {
+            const member = message.guild.member(user);
+            if (member) {
+                if (!member.voiceChannel) {
+                    message.channel.send(`${member.user.username} is not connected to voice.`)
+                } else if (member.voiceChannel !== toVoiceChannel) {
+                    member.setVoiceChannel(toVoiceChannel);
+                }
+            }
+        });
+    }
+};
