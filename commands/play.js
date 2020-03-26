2bea60bf883425813ef52d0970c6af6edb6ed42f
Giovani Vercauteren <giovani.vercauteren@gmail.com>
2020-03-26 22:20:13 +0100

    Optimized play code

--- commands/play.js
+++ commands/play.js
@@ -4,7 +4,8 @@ const Enmap = require('enmap');
 
 let soundMappings;
 let currentTimeout;
-let busy = false;
+let queue = [];
+let processingQueue = false;
 
 exports.setup = (client) => {
     fs.readdir('./sounds/', (err, files) => {
@@ -14,7 +15,8 @@ exports.setup = (client) => {
         }
         files.forEach(file => {
             const soundName = file.split('.')[0];
-            soundMappings.set(soundName, file);
+            const path = `./sounds/${file}`;
+            soundMappings.set(soundName, path);
         });
     });
 };
@@ -32,66 +34,42 @@ exports.run = (client, message, args) => {
     const authorUser = message.author;
     const authorMember = message.guild.member(authorUser);
     const toVoiceChannel = authorMember.voice.channel;
-    const soundname = args[0];
+    const broadcastName = args[0];
 
-    if (soundname === 'list') {
+    if (broadcastName === 'list') {
         let result = '';
         soundMappings.keyArray().forEach(item => result += `${item}, `);
         message.channel.send(result);
         return;
-    } else if (soundname === 'stop') {
-        toVoiceChannel.leave();
-        busy = false;
+    } else if (broadcastName === 'stop') {
+        processQueue(client);
+        return;
+    } else if (soundMappings.has(broadcastName)) {
+        let path = soundMappings.get(broadcastName);
+        queue.push({toVoiceChannel, path});
+    } else {
+        message.channel.send(`'${broadcastName}' is not a valid argument.`)
         return;
     }
 
-    if (busy) {
-        message.channel.send('I am currently busy. Try again in a sec.');
-        return;
-    } else {
-        busy = true;
-    }
-
-    let filename;
-    if (soundMappings.has(soundname)) {
-        filename = soundMappings.get(soundname);
-    }
-    if (toVoiceChannel != null && filename) {
-        toVoiceChannel.join()
-            .then(connection => {
-                if (currentTimeout) clearTimeout(currentTimeout);
-                currentTimeout = setTimeout(() => {
-                    finish(connection);
-                }, 1000 * 60 * 15);
-                try {
-                    const path = `./sounds/${filename}`;
-                    const dispatcher = connection.play(path);
-                    dispatcher.on('finish', () => {
-                        finish(connection);
-                    });
-                } catch (e) {
-                    console.error(e);
-                    connection.disconnect();
-                    busy = false;
-                }
-            }).catch(err => {
-            console.log(err);
-            toVoiceChannel.leave();
-            busy = false;
-        });
-    } else {
-        message.channel.send(`${soundname} is not a valid sound name`);
-        busy = false;
+    if (!processingQueue) {
+        processingQueue = true;
+        processQueue(client);
     }
 };
 
-
-function finish(connection) {
-    if (connection.status !== 4) {
-        connection.disconnect();
+function processQueue(client) {
+    if (queue.length === 0) {
+        processingQueue = false;
+        client.voice.connections.array().forEach(connection => connection.disconnect());
+    } else {
+        const nextItemInQueue = queue.shift();
+        nextItemInQueue.toVoiceChannel.join().then(connection => {
+            const dispatcher = connection.play(nextItemInQueue.path);
+            dispatcher.on('finish', () => {
+                processQueue(client);
+            });
+        }).catch(console.error);
     }
-    clearTimeout(currentTimeout);
-    currentTimeout = undefined;
-    busy = false;
 }
 
