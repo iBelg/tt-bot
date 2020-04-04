5aaa4935d82b16bb11beb99d1f3aab6f0d2b0b80
Giovani Vercauteren <giovani.vercauteren@gmail.com>
2020-04-04 19:06:44 +0200

    Added git webhook

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
 require('dotenv').config();
+require('./webhook');
 const bot = require('./bot.js');
 bot();
---
+++ webhook.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const crypto = require('crypto');
+const exec = require('child_process').exec;
+const webhookSecret = process.env.WEBHOOK_SECRET;
+const pathOfRepo = process.env.REPO_PATH;
+const port = process.env.OPEN_PORT;
+
+http.createServer((req, res) => {
+   req.on('data', (chunk) => {
+       let sig = `sha1${crypto.createHmac('sha1', webhookSecret).update(chunk.toString()).digest('hex')}`;
+       if (req.headers['x-hub-signature'] === sig) {
+           exec(`cd ${pathOfRepo} && git reset --hard HEAD && git pull`);
+       }
+   });
+
+   res.end();
+}).listen(port);
